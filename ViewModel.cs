736a4a481d31b3123c4f3f92b6e02cc95bf6429c
Tamas Vass <tamas.vass@synergysports.com>
2022-11-25 12:23:35 +0100

    Initial version

---
+++ ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.Threading;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+
+namespace AvaloniaApp
+{
+    public class ViewModel : ReactiveObject
+    {
+        public ViewModel()
+        {
+            TestCommand = ReactiveCommand.Create(OnTest);
+        }
+
+        public ReactiveCommandBase<Unit, Unit> TestCommand { get; }
+
+        private void OnTest()
+        {
+            var a = AvaloniaScheduler.Instance;
+            var b = RxApp.MainThreadScheduler;
+        }
+    }
+}
